8e36fc4fd2089b0f1b750d3b33308160e7aa94b3
insua <insuaxe@gmail.com>
2017-05-05 11:48:59 +0800

    commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,3 +17,19 @@ Route::get('/', function () {
 
 Route::get('blog','BlogController@index')->name('blog.index');
 Route::get('blog/{slug}','BlogController@showPost')->name('blog.showPost');
+
+//Admin area
+Route::get('admin',function(){
+    return redirect();
+});
+
+Route::group(['namespace' => 'Admin','middleware'=> 'auth'],function (){
+    Route::resource('admin/post','PostController');
+    Route::resource('admin/tag','TagController');
+    Route::get('admin/upload','UploadController@index');
+});
+
+//logging in and out
+Route::get('auth/login','Auth\AuthController@getLogin')->name('login');
+Route::post('auth/login','Auth\AuthController@postLogin')->name('login');
+Route::get('auth/logout','Auth\AuthController@getLogout')->name('logout');
