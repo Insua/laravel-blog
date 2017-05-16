704f276f97d33dc620d08c75196214379815b20b
insua <insuaxe@gmail.com>
2017-05-16 18:00:46 +0800

    This is where it all begins...

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -88,9 +88,8 @@ class TagController extends Controller
         $data = ['id' => $id];
         foreach(array_keys($this->fields) as $field)
         {
-            $data[$field] = old($field,$tag->field);
+            $data[$field] = old($field,$tag->$field);
         }
-        dd($data);
         return view('admin.tag.edit',$data);
     }
 
