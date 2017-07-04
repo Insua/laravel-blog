cff6a46ab1668eac3169cd050f9b772d386bc461
insua <insuaxe@gmail.com>
2017-07-04 09:27:39 +0800

    pam anderson is going to love me.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Services\Markdowner;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
@@ -16,5 +17,44 @@ class Post extends Model
         {
             $this->attributes['slug'] = str_slug($value);
         }
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Models\Tag','post_tag_pivot');
+    }
+
+    protected function setUniqueSlug($title,$extra)
+    {
+        $slug = str_slug($title.'-'.$extra);
+
+        if(static::whereSlug($slug)->exists())
+        {
+            $this->setUniqueSlug($title,$extra + 1);
+            return;
+        }
+
+        $this->attributes['slug'] = $slug;
+    }
+
+    public function setContentRawAttribute($value)
+    {
+        $markdown = new Markdowner();
+
+        $this->attributes['content_raw'] = $value;
+        $this->attributes['content_html'] = $markdown->toHTML($value);
+    }
+
+    public function syncTags(array $tags)
+    {
+        Tag::addNeededTags($tags);
+
+        if(count($tags))
+        {
+            $this->tags()->sync(Tag::whereIn('tag',$tags)->lists('id')->all());
+            return;
+        }
+
+        $this->tags()->detach();
     }
 }
